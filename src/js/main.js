08d60e6d9adf54592365f5e845cfe54c0c451b20
dubois64 <constance.dubois@studentfr.ch>
2025-01-22 17:04:08 +0100

    tentative exercice 1

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-var button = document.querySelector(".change-color");
+var button = document.toggle(".change-color");
 document.querySelector(".get-blue");
 
 var openMenu = function () {
@@ -7,3 +7,41 @@ var openMenu = function () {
 };
 
 button.addEventListener("click", openMenu);
+
+yellow.classList.add("is-active");
+
+//tkt...
+console.log("change-color");
+
+var price = 35;
+console.log(price);
+
+var firstname = "Nicolas";
+console.log("Bonjour " + firstname);
+
+function sayHello(firstname, lastname) {
+  console.log("Hello" + firstname + " " + lastname);
+}
+
+console.log(window.innerHeight);
+
+console.log(document.querySelector(".change-color"));
+var button = document.querySelector(".change-color");
+var message = document.querySelector(".change-color");
+
+console.log(button.innerHTML);
+console.log(button.clientHeight);
+
+function sayGoodbye() {
+  box.innerHTML = "Goodbye !";
+  box.classList.toggle("is-active");
+  box.classList.add("is-visible");
+}
+
+change - color.addEventListener("mouseenter", sayGoodbye);
+
+function changeColor() {
+  console.log("Blue");
+}
+
+changeColor(blue);

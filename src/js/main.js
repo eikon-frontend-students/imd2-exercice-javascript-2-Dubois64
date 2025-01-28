c051e3dc6c25d43bc316393b523c574845cf1b40
dubois64 <constance.dubois@studentfr.ch>
2025-01-28 10:13:34 +0100

    exercice 1

--- src/js/main.js
+++ src/js/main.js
@@ -1,47 +1,67 @@
-var button = document.toggle(".change-color");
-document.querySelector(".get-blue");
+// var button = document.toggle(".change-color");
+// document.querySelector(".get-blue");
 
-var openMenu = function () {
-  menu.classList.toggle("is-open");
-  button.classList.toggle("is-active");
-};
+// var openMenu = function () {
+//   menu.classList.toggle("is-open");
+//   button.classList.toggle("is-active");
+// };
 
-button.addEventListener("click", openMenu);
+// button.addEventListener("click", openMenu);
 
-yellow.classList.add("is-active");
+// yellow.classList.add("is-active");
 
-//tkt...
-console.log("change-color");
+// //tkt...
+// console.log("change-color");
 
-var price = 35;
-console.log(price);
+// var price = 35;
+// console.log(price);
 
-var firstname = "Nicolas";
-console.log("Bonjour " + firstname);
+// var firstname = "Nicolas";
+// console.log("Bonjour " + firstname);
 
-function sayHello(firstname, lastname) {
-  console.log("Hello" + firstname + " " + lastname);
-}
+// function sayHello(firstname, lastname) {
+//   console.log("Hello" + firstname + " " + lastname);
+// }
 
-console.log(window.innerHeight);
+// console.log(window.innerHeight);
 
-console.log(document.querySelector(".change-color"));
-var button = document.querySelector(".change-color");
-var message = document.querySelector(".change-color");
+// console.log(document.querySelector(".change-color"));
+// var button = document.querySelector(".change-color");
+// var message = document.querySelector(".change-color");
 
-console.log(button.innerHTML);
-console.log(button.clientHeight);
+// console.log(button.innerHTML);
+// console.log(button.clientHeight);
 
-function sayGoodbye() {
-  box.innerHTML = "Goodbye !";
-  box.classList.toggle("is-active");
-  box.classList.add("is-visible");
-}
+// function sayGoodbye() {
+//   box.innerHTML = "Goodbye !";
+//   box.classList.toggle("is-active");
+//   box.classList.add("is-visible");
+// }
 
-change - color.addEventListener("mouseenter", sayGoodbye);
+// change - color.addEventListener("mouseenter", sayGoodbye);
+
+// function changeColor() {
+//   console.log("Blue");
+// }
+
+// changeColor(blue);
+
+var box1 = document.querySelector(".change-color");
 
 function changeColor() {
-  console.log("Blue");
+  box1.classList.add("is-blue");
+  // box1.classList.toggle("is-blue");
 }
 
-changeColor(blue);
+box1.addEventListener("click", changeColor);
+
+// box 2
+
+var box2 = document.querySelector(".change-color");
+console.log;
+
+function ChangeColor() {
+  box2.classList.toggle("is-red-orange");
+}
+
+box2.addEventListener("click", changeColor);
